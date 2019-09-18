c883282c4ae28aa71bd91bf53917a646ef2c1d02
carl svensson <lordcarlviktor@hotmail.com>
2019-09-18 15:14:58 +0200

    la till clear screen

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -1,3 +1,4 @@
+import os
 konto = {}
 
 def KollaNummer(text):
@@ -6,22 +7,26 @@ def KollaNummer(text):
             val = int(input(text))
             return val
         except:
-            print("Ej tillgängligt, använd siffror. ")
+            print("Ej tillgängligt, använd siffror")
 
 def Meny1():
     nummer = KollaNummer("Ange kontonummer: ")
     if nummer in konto:
-        print("Konto finns redan. ")
+        os.system('cls')
+        print("Konto {nummer} finns redan. ")
     else:
         konto[nummer]= 0
+        os.system('cls')
         print(f"Konto {nummer} skapat. ")
 
 
 def Meny2():
     nummer = int(input("Ange kontonummer: "))
     if nummer not in konto:
+        os.system('cls')
         print("Konto finns ej. ")
         return
+    os.system('cls')
     print(f"Välkommen {nummer}")
     saldo = konto[nummer]
     while True:
@@ -31,9 +36,11 @@ def Meny2():
         print("3. Visa saldo")
         print("4. Logga ut")
         val = KollaNummer("Välj: ")
+        os.system('cls')
         if val == 1:
             uttag = KollaNummer("Ange belopp: ")
             if saldo >= uttag and uttag > 0:
+                os.system('cls')
                 print(f"{uttag} uttaget. ")
                 saldo = saldo - uttag
                 konto[nummer] = saldo
@@ -42,6 +49,7 @@ def Meny2():
         elif val == 2:
             insatt = KollaNummer("Ange belopp: ")
             if insatt > 0:
+                os.system('cls')
                 print(f"{insatt} insatt på ditt konto. ")
                 saldo = saldo + insatt
                 konto[nummer] = saldo
@@ -61,6 +69,7 @@ while True:
     print("2. Logga in konto")
     print("3. Avsluta")
     val = KollaNummer("Välj: ")
+    os.system('cls')
     if val == 1:
         Meny1()
     elif val == 2:
